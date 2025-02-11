c132b1a816d8b21a237cf0d98922933c1ce33c9b
sandraliv <sls40@hi.is>
2025-02-11 17:49:50 +0000

    new app

---
+++ app/src/main/java/com/hi/recipeapp/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,47 @@
+package com.hi.recipeapp.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.hi.recipeapp.databinding.FragmentNotificationsBinding
+
+//This class extends Fragment(), meaning it represents a reusable UI component.
+class NotificationsFragment : Fragment() {
+
+    // _binding holds the view binding reference for the fragment
+    private var _binding: FragmentNotificationsBinding? = null
+
+    // binding is a non-nullable property, ensuring safe access to UI elements within the fragment's lifecycle
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    // _binding is set to null in onDestroyView() to prevent memory leak
+    //If an Android Fragment, memory leaks can occur if the fragments holds references to UI elemnts (like TextView, Buttons, etc) AFTER the view is destroyed.
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
