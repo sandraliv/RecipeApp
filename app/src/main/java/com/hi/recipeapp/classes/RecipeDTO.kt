0ea44c10ba983edf618eb610a5a679594d4c85c0
Ásdís Halldóra L Stefánsdóttir <103222479+asdis96@users.noreply.github.com>
2025-02-16 18:12:25 +0000

    Update RecipeDTO.kt

--- app/src/main/java/com/hi/recipeapp/classes/RecipeDTO.kt
+++ app/src/main/java/com/hi/recipeapp/classes/RecipeDTO.kt
@@ -3,7 +3,7 @@ package com.hi.recipeapp.classes
 import com.google.gson.annotations.SerializedName
 
 data class RecipeCard(
-    @SerializedName("image_url") val imageUrl: String, 
+    @SerializedName("image_url") val imageUrl: String,
     val title: String,
     val description: String,
     val averageRating: Double,
